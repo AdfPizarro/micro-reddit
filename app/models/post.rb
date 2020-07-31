6d05702f30f9a0363a79393bcbc71a5b22be4556
Nnamdi <emelunna@gmail.com>
2020-07-31 18:31:19 +0100

    creating the comment model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
 	validates :title, :body, presence: true
 	belongs_to :user
+	has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
 	validates :email, :presence => true
 	validates :username, :presence => true
 	validates :password, :presence => true
-	
+	has_many :comments
 	has_many :posts
 end
